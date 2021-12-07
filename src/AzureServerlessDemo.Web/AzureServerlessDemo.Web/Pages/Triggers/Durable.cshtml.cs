b895602c6a21ee97a12489f6f267a89a67c8e6f4
Bojan Vrhovnik <bojan@vrhovnik.net>
2021-12-07 13:09:31 +0100

    add version of nuget to load .NET 6 option

---
+++ src/AzureServerlessDemo.Web/AzureServerlessDemo.Web/Pages/Triggers/Durable.cshtml.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace AzureServerlessDemo.Web.Pages.Triggers;
+
+public class DurablePageModel : PageModel
+{
+    private readonly ILogger<DurablePageModel> logger;
+
+    public DurablePageModel(ILogger<DurablePageModel> logger)
+    {
+        this.logger = logger;
+    }
+
+    [BindProperty]
+    public string SimpleDurableUrl { get; set; }
+
+    [BindProperty]
+    public string FanInOutDurableUrl { get; set; }
+
+    public void OnGet()
+    {
+        logger.LogInformation("Page DurablePageModel loaded");
+        SimpleDurableUrl = Environment.GetEnvironmentVariable("SimpleDurableUrl");
+        logger.LogInformation("Simple durable url: " + SimpleDurableUrl);
+        FanInOutDurableUrl = Environment.GetEnvironmentVariable("FanInOutDurableUrl");
+        logger.LogInformation("Fan in/out durable url: " + FanInOutDurableUrl);
+    }
+}
